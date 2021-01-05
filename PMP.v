4c8a12086447d6121a6144a4bc4ee561a1284a1b
abisubramanya27 <abisubramanya27@gmail.com>
2021-01-05 21:32:53 +0530

    small change

--- PMP.v
+++ PMP.v
@@ -42,43 +42,7 @@ module PMP(
             assign data_accepted[j] = 0;
             assign pattern_accepted[j] = 0;
         end
-    endgenerate
-
-    // PMM m1(   
-    //     .clk(clk),
-    //     .INP_DATA(pmp_data[0]),
-    //     .INP_CONTROL(pmp_control[0]),
-    //     .DATA_VALID(pmp_data_ready[0]),
-    //     .READY_STATUS(data_accepted[0]),
-    //     .ACCEPTED_STATUS(pattern_accepted[0])
-    // );
-     
-    // PMM m2(  
-    //     .clk(clk), 
-    //     .INP_DATA(pmp_data[1]),
-    //     .INP_CONTROL(pmp_control[1]),
-    //     .DATA_VALID(pmp_data_ready[1]),
-    //     .READY_STATUS(data_accepted[1]),
-    //     .ACCEPTED_STATUS(pattern_accepted[1])
-    // );
-    
-    // PMM m3(   
-    //     .clk(clk),
-    //     .INP_DATA(pmp_data[2]),
-    //     .INP_CONTROL(pmp_control[2]),
-    //     .DATA_VALID(pmp_data_ready[2]),
-    //     .READY_STATUS(data_accepted[2]),
-    //     .ACCEPTED_STATUS(pattern_accepted[2])
-    // );
-    
-    // PMM m4(   
-    //     .clk(clk),
-    //     .INP_DATA(pmp_data[3]),
-    //     .INP_CONTROL(pmp_control[3]),
-    //     .DATA_VALID(pmp_data_ready[3]),
-    //     .READY_STATUS(data_accepted[3]),
-    //     .ACCEPTED_STATUS(pattern_accepted[3])
-    // );    
+    endgenerate   
     
     always @(posedge clk) begin
         for(i=0; i <`NO_MODULES; i=i+1) begin
